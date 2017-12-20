cd36102fa424ead7a310d272243c84f2474285c4
rathereasy@gmail.com <jacques@decodemtl>
2017-12-19 23:37:40 -0500

    Question 2

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
+var bodyParser = require('body-parser')
+app.use(bodyParser.raw({ type: '*/*' }))
 
-app.get('/todos', (req, res) => res.send(JSON.stringify(["breath", "eat", "sleep"])))
+let todos = ["breath", "eat", "sleep"]
+
+app.get('/todos', (req, res) => res.send(JSON.stringify(todos)))
+
+app.post('/addTodo', (req, res) => {
+    todos.push(JSON.parse(req.body.toString()));
+    res.send("ok")
+})
 
 app.listen(3000, () => console.log('Port 3000!'))
