fdc538a4d46e71b29bdc1411530fb292c59a5838
rathereasy@gmail.com <jacques@decodemtl>
2017-12-20 20:23:31 -0500

    Question 7

--- server.js
+++ server.js
@@ -4,17 +4,30 @@ const cors = require('cors');
 var bodyParser = require('body-parser')
 app.use(bodyParser.raw({ type: '*/*' }))
 app.use(cors())
-let todos = ["breath", "eat", "sleep"]
+let todos = {}
 
-app.get('/todos', (req, res) => res.send(JSON.stringify(todos)))
+app.get('/todos', (req, res) => {
+    let username = req.query.username
+    let userTodos = todos[username]
+    if(userTodos == undefined) userTodos = []
+    res.send(JSON.stringify(userTodos))
+})
 
 app.post('/addTodo', (req, res) => {
-    todos.push(JSON.parse(req.body.toString()));
+    let payload = JSON.parse(req.body.toString());
+    let item = payload.item;
+    let username = payload.username;
+    let userTodos = todos[username]
+    if(userTodos == undefined) userTodos = []
+    userTodos.push(item);
+    todos[username] = userTodos;
     res.send("ok")
 })
 
 app.post('/clearTodos', (req, res) => {
-    todos = [];
+    let payload = JSON.parse(req.body.toString());
+    let username = payload.username;
+    todos[username] = [];
     res.send("ok")
 })
 
